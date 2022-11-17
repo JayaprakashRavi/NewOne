f5321a9ea91bd9b52de5ea99ee7c350ea49319fc
JayaprakashRavi <rjayaprakashmech1997@gmail.com>
2022-11-17 19:50:07 +0530

    hai

---
+++ src/org/test/Test1.java
@@ -0,0 +1,6 @@
+package org.test;
+
+public class Test1 {
+	public Test1() {System.out.println("parent class test1");}
+
+}
---
+++ src/org/test/Test2.java
@@ -0,0 +1,8 @@
+package org.test;
+
+public class Test2 extends Test1{
+	public Test2() {
+		super();
+		System.out.println("child class test2");
+	}
+}
